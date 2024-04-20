28ad65dfd75b04cf1870e0b2edac9ce49afe03de
ZALOFARG <andre.work.go@gmail.com>
2024-04-20 13:29:27 -0500

    feat: script creation that allows the retrieval of a user's todo list

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Script that retrieves and displays the progress of an
+employee's TODO list
+"""
+
+import requests
+from sys import argv
+
+employee_id = int(argv[1])
+
+base_url = 'https://jsonplaceholder.typicode.com/'
+url = 'https://jsonplaceholder.typicode.com/todos?userId='
+
+user_response = requests.get(f'{base_url}/users/{employee_id}')
+user_data = user_response.json()
+user_name = user_data.get('name')
+
+todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+todos_data = todos_response.json()
+
+total_tasks = len(todos_data)
+done_tasks = [task for task in todos_data if task['completed']]
+num_done_tasks = len(done_tasks)
+
+print('Employee {} is done with ({}/{}):'.format(
+    user_name, num_done_tasks, total_tasks)
+    )
+for task in done_tasks:
+    print(f"\t{task['title']}")
