49e8b8ca30fa592a20baf3b23ee91386d1e1ba84
ZALOFARG <andre.work.go@gmail.com>
2024-04-20 13:44:56 -0500

    docs: funct documentation following PEP8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,24 +7,34 @@ employee's TODO list
 import requests
 from sys import argv
 
-employee_id = int(argv[1])
 
-base_url = 'https://jsonplaceholder.typicode.com/'
-url = 'https://jsonplaceholder.typicode.com/todos?userId='
+def get_employee_todo_list(employee_id):
+    """Fetches and displays the employee's todo list
 
-user_response = requests.get(f'{base_url}/users/{employee_id}')
-user_data = user_response.json()
-user_name = user_data.get('name')
+    Args:
+        employee_id(int): the ID of the employee
+    """
 
-todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
-todos_data = todos_response.json()
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    url = 'https://jsonplaceholder.typicode.com/todos?userId='
 
-total_tasks = len(todos_data)
-done_tasks = [task for task in todos_data if task['completed']]
-num_done_tasks = len(done_tasks)
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    user_data = user_response.json()
+    user_name = user_data.get('name')
 
-print('Employee {} is done with ({}/{}):'.format(
-    user_name, num_done_tasks, total_tasks)
-    )
-for task in done_tasks:
-    print(f"\t{task['title']}")
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos_data = todos_response.json()
+
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task['completed']]
+    num_done_tasks = len(done_tasks)
+
+    print('Employee {} is done with ({}/{}):'.format(
+        user_name, num_done_tasks, total_tasks)
+        )
+    for task in done_tasks:
+        print(f"\t{task['title']}")
+
+if __name__ == '__main__':
+    employee_id = int(argv[1])
+    get_employee_todo_list(employee_id)
